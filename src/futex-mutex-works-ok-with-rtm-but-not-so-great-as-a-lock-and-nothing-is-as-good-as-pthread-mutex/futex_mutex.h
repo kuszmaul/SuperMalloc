68c4dbd5e40588d4b63c8bec2cdb133c01b6be22
Bradley C. Kuszmaul <bradley@tokutek.com>
2014-10-12 16:09:00 -0400

    Implement using our mutex pthread cond variables

--- src/futex-mutex-works-ok-with-rtm-but-not-so-great-as-a-lock-and-nothing-is-as-good-as-pthread-mutex/futex_mutex.h
+++ src/futex-mutex-works-ok-with-rtm-but-not-so-great-as-a-lock-and-nothing-is-as-good-as-pthread-mutex/futex_mutex.h
@@ -1,9 +1,6 @@
 #ifndef FUTEX_MUTEX_H
 #define FUTEX_MUTEX_H
 
-#include <stdint.h>
-#include <pthread.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,14 +9,11 @@ extern "C" {
 
 // See futex_mutex.cc for the meaning of these fields.
 typedef struct futex_mutex_s {
-  volatile int locked;
-  volatile uint64_t n_locking;
-  volatile uint64_t n_waiting;
-  pthread_mutex_t mutex;
-  pthread_cond_t  locking, waiting;
+  volatile int lock __attribute__((aligned(64)));
+  volatile int wait __attribute__((aligned(64)));
 } futex_mutex_t;
 
-#define FUTEX_MUTEX_INITIALIZER {0,0,0,PTHREAD_MUTEX_INITIALIZER,PTHREAD_COND_INITIALIZER,PTHREAD_COND_INITIALIZER}
+#define FUTEX_MUTEX_INITIALIZER {0,0}
 int futex_mutex_lock(futex_mutex_t *m); // return 0 if it's a fast lock, 1 if it's slow.
 void futex_mutex_unlock(futex_mutex_t *m);
 int futex_mutex_subscribe(futex_mutex_t *m); // (return 0 if it is unlocked)
--- src/futex_mutex.cc
+++ src/futex_mutex.cc
@@ -1,6 +1,4 @@
 #include <assert.h>
-#include <linux/futex.h>
-#include <sys/syscall.h>
 #include <limits.h>
 #include <unistd.h>
 #include <immintrin.h>
@@ -10,113 +8,51 @@
 
 #include "futex_mutex.h"
 
-// the lock field:  bit 1 is the lock
-//                  bit 2 says someone is "waiting"
-//                  the rest of the bits are the count for the number waiting to lock.
-const int increment_locker_by = 4;
-
-static long sys_futex(void *addr1, int op, int val1, struct timespec *timeout, void *addr2, int val3)
-{
-  return syscall(SYS_futex, addr1, op, val1, timeout, addr2, val3);
-}
-
-static long futex_wait(volatile int *addr, int val) {
-  return sys_futex((void*)addr, FUTEX_WAIT_PRIVATE, val, NULL, NULL, 0);
-}
-static long futex_wake1(volatile int *addr) {
-  return sys_futex((void*)addr, FUTEX_WAKE_PRIVATE, 1,   NULL, NULL, 0);
-}
-static long futex_wakeN(volatile int *addr) {
-  return sys_futex((void*)addr, FUTEX_WAKE_PRIVATE, INT_MAX,   NULL, NULL, 0);
-}
-
 static const int lock_spin_count = 100;
 static const int unlock_spin_count = 200;
 
 // Return 0 if it's a fast acquiistion, 1 if slow
 extern "C" int futex_mutex_lock(futex_mutex_t *m) {
-  int count = 0;
-  while (count < lock_spin_count) {
-    int old_c = m->lock;
-    if ((old_c & 1) == 1) {
-      // Someone else has the lock, so we are spinning.
-      _mm_pause();
-      count++;
-    } else if (__sync_bool_compare_and_swap(&m->lock, old_c, old_c | 1)) {
-      // No one else had the lock, and we successfully grabbed it.
-      return 0;
-    } else {
-      // Someone else modified old_c while we were running.  So we just want to try again, without incrementing count or pausing
-      continue;
-    }
+  pthread_mutex_lock(&m->mutex);
+  m->n_locking++;
+  while (m->locked) {
+    pthread_cond_wait(&m->locking, &m->mutex);
   }
-  
-  // We got here without getting the lock, so let's add ourselves to the count.
-  __sync_fetch_and_add(&m->lock, increment_locker_by);
-
-  // Now we must wait for the lock to go free.  We'll use the futex, but we'll be opportunistic if value changed.
-  bool did_futex = false;
-  while (1) {
-    int old_c = m->lock;
-    if ((old_c & 1) == 1) {
-      // Someone else has the lock
-      futex_wait(&m->lock, old_c); // we don't care if the futex fails because old_c changed, we'll just go again anyway.
-      did_futex = true;
-    } else if (__sync_bool_compare_and_swap(&m->lock, old_c, old_c-increment_locker_by +1)) {
-      // No one else had the lock, and we managed to grab it (decrementing by 3 has the effect of subtracting 4 (to indicate that we are no longer waiting) and setting the lock bit.
-      return did_futex;
-    } else {
-      // No one else had the lock, but someone modified it while we were trying to lock, so just try again without pausing
-      continue;
-    }
-  }
-}   
-  
+  m->n_locking--;
+  m->locked = true;
+  pthread_mutex_unlock(&m->mutex);
+  return 0;
+}
+ 
 extern "C" void futex_mutex_unlock(futex_mutex_t *m) {
-  while (1) {
-    int old_m = m->lock;
-    if (__sync_bool_compare_and_swap(&m->lock, old_m, old_m & ~1)) {
-      if ((old_m & ~3) == 0) {
-	// No one was waiting to lock, so it's unlocked with no contention.  
-	if (m->wait) {
-	  // Wake up anyone who is waiting for 0.
-	  m->wait = 0;
-	  __sync_fetch_and_and(&m->lock, ~2); // no one is waiting on the lock now.
-	  futex_wakeN(&m->wait);
-	}
-	return;
-      } else {
-	break;             // someone may be waiting, so we'll have to wake them up.
-      }
-    }
+  pthread_mutex_lock(&m->mutex);
+  m->locked = 0;
+  if (m->n_locking) {
+    pthread_cond_signal(&m->locking);
+  } else if (m->n_waiting) {
+    pthread_cond_broadcast(&m->waiting);
   }
-  // Spin a little, hoping that someone takes the lock.
-  for (int i = 0; i < unlock_spin_count; i++) {
-    if (m->lock & 1) return; // someone else took the lock, so it will wake anyone that needs waking when it's done.
-    _mm_pause();
-  }
-  // No one took it, so we have to wake someone up.
-  futex_wake1(&m->lock);
+  pthread_mutex_unlock(&m->mutex);
 }
 
 extern "C" int futex_mutex_subscribe(futex_mutex_t *m) {
-  return (m->lock)&1;
+  return m->locked;
 }
 
 extern "C" int futex_mutex_wait(futex_mutex_t *m) {
-  while (1) {
-    for (int count = 0; count < lock_spin_count; count++) {
-      if ((m->lock & 1) == 0) return false; // it was quick
-      _mm_pause();
-    }
-    // So we resign ourselves to waiting
-    
-    if ((m->lock & 2) == 0) {
-      __sync_fetch_and_or(&m->lock, 2); // tell the lock I'm waiting
-    }
-    m->wait = 1;
-    futex_wait(&m->wait, 1);
+  for (int i = 0; i < lock_spin_count; i++) {
+    if (!m->locked) return 0; //  We can be a little opportunistic here.
+    _mm_pause();
   }
+  // Now we have to do the relatively heavyweight thing.
+  pthread_mutex_lock(&m->mutex);
+  m->n_waiting++;
+  while (m->locked) {
+    pthread_cond_wait(&m->waiting, &m->mutex);
+  }
+  m->n_waiting--;
+  pthread_mutex_unlock(&m->mutex);
+  return 1;
 }
   
 #ifdef TESTING
@@ -184,7 +120,7 @@ static void stress() {
 	  } else {
 	    wait_short++;
 	  }
-	  if (m. lock & 1) {
+	  if (m.locked) {
 	    wait_was_one++;
 	  } else {
 	    wait_was_zero++;
