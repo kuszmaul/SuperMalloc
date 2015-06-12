787d4efd4d3329ae67188e79781fc92d7a0d1053
Bradley C. Kuszmaul <bradley@tokutek.com>
2015-06-12 15:55:04 -0400

    Investigate the performance slowdown shown by profile_malloc

--- src/malloc_internal.h
+++ src/malloc_internal.h
@@ -177,5 +177,9 @@ struct per_folio {
 #include "unit-tests.h"
 #endif
 
+static inline double tdiff(struct timespec *start, struct timespec *end) {
+  return end->tv_sec - start->tv_sec + 1e-9*(end->tv_nsec - start->tv_nsec);
+}
+
 #endif
 
